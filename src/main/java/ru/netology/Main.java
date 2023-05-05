35ece31259a25e7f6068a3130f5da0287c2a8c79
lachtak <lazynewocean@gmail.com>
2023-05-05 17:00:29 +0300

    fix for types in FormDate

--- src/main/java/ru/netology/Main.java
+++ src/main/java/ru/netology/Main.java
@@ -13,9 +13,9 @@ public class Main {
         post.subscription = true;
 
         post.birthday = new FormDate();
-        post.birthday.day = "14";
-        post.birthday.month = "04";
-        post.birthday.year = "1993";
+        post.birthday.day = 14;
+        post.birthday.month = 4;
+        post.birthday.year = 1993;
 
     }
 }
